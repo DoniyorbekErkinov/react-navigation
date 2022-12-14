85dae1d51dce85845e4a89972e102dd4c21c30a9
doniyor <erkinovdoniyor03@gmail.com>
2022-12-14 12:20:41 +0500

    cleaned

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import Navbar from "./Navbar";
 import './styles.css'
 import Home from "./pages/Home";
-import Footer from "./Footer";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Courses from "./pages/Courses";
 function App() {
   return (
     <>
@@ -13,11 +10,8 @@ function App() {
             <Route path="/" element={<Navbar/>}>
                 <Route index element={<Home/>}/>
                 <Route path='/about' element={<About/>}/>
-                <Route path='/courses' element={<Courses/>}/>
-                <Route path='/contact' element={<Contact/>}/>
             </Route>
         </Routes>
-        <Footer/>
     </>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,13 @@
 import {Fragment} from "react";
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 
 function Navbar() {
     return (
         <Fragment>
 
         <nav className="nav">
-            <a href="/" className="site-title">Panda</a>
-            <ul>
-                <li className="active"><a href="/about" >About</a></li>
-                <li><a href="/courses">Courses</a></li>
-                <li><a href="/contact">Contact us</a></li>
-            </ul>
+            <Link to="/" >Home</Link>
+            <Link to="/about" >About</Link>
         </nav>
             <Outlet/>
         </Fragment>
